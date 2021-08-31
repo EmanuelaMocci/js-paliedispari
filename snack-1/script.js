a05d960e5071d770bffd440c108292aff00b3c2a
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-08-31 15:39:37 +0200

    Aggiunta prompt

---
+++ snack-1/script.js
@@ -0,0 +1,13 @@
+// // 1. Chiedere all’utente di inserire una parola
+var parola = prompt("Inserisci una parola");
+
+// // 2. Creare una funzione per capire se la parola inserita è palindroma
+
+// function palindrome(s) {
+//     var reverseString = s.split("").reverse().join("");
+//     if(s==reverseString)
+//        console.log("palindrome");
+//     else
+//        console.log("not palindrome");
+//  }
+//  palindrome(parola);
---
+++ snack-2/script.js
@@ -0,0 +1,14 @@
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+var numero1 = prompt("Scegli se inserire un numero pari o dispari");
+
+if (numero1 == "dispari") {
+    var numero2 = prompt("Scegli un numero da 1 a 5");
+} else if (numero1 == "pari"){
+    var numero2 = prompt("Scegli un numero da 1 a 5");  
+} else {
+    alert("Errore");
+}
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
