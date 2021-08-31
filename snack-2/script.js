317473dd10c3a3612761fab5380741368488d83d
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-08-31 16:40:51 +0200

    Aggiunta generatore di numero random e somma dei due numeri

--- snack-2/script.js
+++ snack-2/script.js
@@ -4,11 +4,30 @@ var numero1 = prompt("Scegli se inserire un numero pari o dispari");
 if (numero1 == "dispari") {
     var numero2 = prompt("Scegli un numero da 1 a 5");
 } else if (numero1 == "pari"){
-    var numero2 = prompt("Scegli un numero da 1 a 5");  
+    var numero2 = parseInt(prompt("Scegli un numero da 1 a 5"));  
 } else {
     alert("Errore");
 }
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+function randomNumber(min, max){
+    var random = Math.floor(Math.random() * (max - min + 1) + min);
+    return random;
+} 
+console.log(randomNumber(1, 5));
+console.log(numero2);
+
 // Sommiamo i due numeri
+var primoNumero = randomNumber(1, 5);
+var somma = numero2 + primoNumero;
+console.log(somma);
+
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// function pariDispari(somma){
+//     if (somma % 2 == 0) {
+//         alert("La somma dei due numeri è pari");
+//     } else if {
+//         alert("La somma dei due numeri è dispari");
+//     }
+// }
+
 // Dichiariamo chi ha vinto.
