0150cfd9892427053ad6df81a419067a76bc0b44
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-08-31 23:13:04 +0200

    Completamento snack-1

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,7 +1,27 @@
 // // 1. Chiedere all’utente di inserire una parola
-var parola = prompt("Inserisci una parola");
+var parolaUtente = prompt("Inserisci una parola");
+
 
 // // 2. Creare una funzione per capire se la parola inserita è palindroma
+function palindroma (parola){
+    var parolaInversa = "";
+    for (var i = parola.length -1; i >= 0; i--) {
+        parolaInversa = parolaInversa + parola.charAt(i);                            
+    }
+    return parolaInversa;
+}
+
+var parolaInversa = palindroma(parolaUtente); 
+
+if (parolaUtente == parolaInversa) {
+    alert("La parola è palindroma");
+    }else {
+        alert("La parola non è palindroma");
+    }
+
+console.log(parolaInversa);
+
+
 
 // function palindrome(s) {
 //     var reverseString = s.split("").reverse().join("");
